fd1e00b7fc172229037d68baa5985dded630298c
Flavia Elias <flaviaelias@id.uff.br>
2019-09-10 01:30:34 -0300

    Início de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProgressSpinnerModule} from "primeng/progressspinner";
+import {LoginComponent} from "./login/login.component";
+import { TesteComponent } from './teste/teste.component';
+import { AlertComponent } from './alert/alert.component';
+import { GuardsComponent } from './guard/guards.component';
+import { HelpersComponent } from './helper/helpers.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TesteComponent,
+    AlertComponent,
+    GuardsComponent,
+    HelpersComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
