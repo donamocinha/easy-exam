dcf3b2d59b16b091c51fbd057ddffe7f14754b16
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-11-20 13:47:49 -0300

    ajeitando list pacient

--- src/app/patient/patient-list/patient-list.component.ts
+++ src/app/patient/patient-list/patient-list.component.ts
@@ -10,16 +10,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { name: 'Hydrogen', email: 'exemplo@email.com', birthday: '10/07/1990', weight: 70, height: 1.65 },
-  { name: 'Helium', email: 'exemplo@email.com', birthday: '20/07/1990', weight: 70, height: 1.65 },
-  { name: 'Lithium', email: 'exemplo@email.com', birthday: '02/07/1990', weight: 70, height: 1.65 },
-  { name: 'Beryllium', email: 'exemplo@email.com', birthday: '40/07/1990', weight: 70, height: 1.65 },
-  { name: 'Boron', email: 'exemplo@email.com', birthday: '06/07/1990', weight: 70, height: 1.65 },
-  { name: 'Carbon', email: 'exemplo@email.com', birthday: '02/07/1990', weight: 70, height: 1.65 },
-  { name: 'Nitrogen', email: 'exemplo@email.com', birthday: '10/07/1990', weight: 70, height: 1.65 },
-  { name: 'Oxygen', email: 'exemplo@email.com', birthday: '20/07/1990', weight: 70, height: 1.65 },
-  { name: 'Fluorine', email: 'exemplo@email.com', birthday: '13/07/1990', weight: 70, height: 1.65 },
-  { name: 'Neon', email: 'exemplo@email.com', birthday: '06/07/1990', weight: 70, height: 1.65 },
+  { name: 'Alice', email: 'alice@email.com', birthday: '10/07/1990', weight: 60, height: 1.75 },
+  { name: 'Sophia', email: 'sophia@email.com', birthday: '20/07/1990', weight: 80, height: 1.65 },
+  { name: 'Miguel', email: 'miguel@email.com', birthday: '02/07/1990', weight: 50, height: 1.60 },
+  { name: 'Arthur', email: 'arthur@email.com', birthday: '40/07/1990', weight: 70, height: 1.85 },
+  { name: 'Helena', email: 'helena@email.com', birthday: '06/07/1990', weight: 90, height: 1.95 },
+  { name: 'Beatriz', email: 'beatriz@email.com', birthday: '02/07/1990', weight: 100, height: 1.75 },
+  { name: 'Valentina', email: 'valentina@email.com', birthday: '10/07/1990', weight: 65, height: 1.50 },
+  { name: 'Heitor', email: 'heitor@email.com', birthday: '20/07/1990', weight: 76, height: 1.67 },
+  { name: 'João', email: 'joao@email.com', birthday: '13/07/1990', weight: 73, height: 1.68 },
+  { name: 'Maria', email: 'maria@email.com', birthday: '06/07/1990', weight: 64, height: 1.95 },
 ];
 
 @Component({
