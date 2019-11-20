408e5cdbc6f36b557f49bc54ff767d4eb8ceefea
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-11-20 13:47:09 -0300

    iniciando home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,28 @@ import {AuthenticationService} from "../services/authentication.service";
 import {UserService} from "../services/user.service";
 import {first} from "rxjs/operators";
 
+export interface PeriodicElement {
+  activity: string;
+  info: string;
+  updated_by: string;
+  doctorType: string;
+  updated_at: string;
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { activity: 'Novo paciente cadastrado', info: 'A paciente Alice foi cadastrada', updated_by: 'Rubi', doctorType: 'Médico', updated_at: '15/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'A paciente Sophia foi cadastrada', updated_by: 'Julio', doctorType: 'Médico e Pesquisador', updated_at: '13/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'O paciente Miguel foi cadastrado', updated_by: 'Daniela', doctorType: 'Médico', updated_at: '12/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'O paciente Arthur foi cadastrado', updated_by: 'Leonardo', doctorType: 'Médico', updated_at: '12/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'A paciente Helena foi cadastrada', updated_by: 'Julio', doctorType: 'Médico e Pesquisador', updated_at: '11/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'A paciente Beatriz foi cadastrada', updated_by: 'Guttenberg', doctorType: 'Médico e Pesquisador', updated_at: '10/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'A paciente Valentina foi cadastrada', updated_by: 'Samiry', doctorType: 'Médico e Pesquisadora', updated_at: '09/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'O paciente Heitor foi cadastrado', updated_by: 'Guttenberg', doctorType: 'Médico e Pesquisador', updated_at: '08/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'O paciente João foi cadastrado', updated_by: 'Irene', doctorType: 'Médico e Pesquisadora', updated_at: '02/11/2019' },
+  { activity: 'Novo paciente cadastrado', info: 'A paciente Maria foi cadastrada', updated_by: 'Rebeca', doctorType: 'Médico', updated_at: '01/11/2019' },
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,6 +37,9 @@ export class HomeComponent implements OnInit {
   currentUser: User;
   currentUserSubscription: Subscription;
   users: User[] = [];
+
+  displayedColumns: string[] = ['activity', 'info', 'updated_by', 'doctorType', 'updated_at'];
+  dataSource = ELEMENT_DATA;
 
   constructor(
     private authenticationService: AuthenticationService,
