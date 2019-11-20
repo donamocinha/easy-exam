ac12570d39b38d375419d2447ce67e7b663529a7
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-11-20 16:58:36 -0300

    postPatients!

--- src/app/patient/patient-form/patient-form.component.ts
+++ src/app/patient/patient-form/patient-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PatientService } from 'src/app/services/patient.service';
 
 
 export interface Patient  {
   id?: string; 
   birthday: string;
+  cpf: string;
   email: string;
-  height: string;
+  height: number;
   name: string;
   sex: string;
-  weight: string;
+  weight: number;
  }
 
 @Component({
@@ -19,24 +21,33 @@ export interface Patient  {
 })
 export class PatientFormComponent implements OnInit {
 
-  form: FormGroup
+  newPatientForm: FormGroup
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private service: PatientService
   ) { }
 
   ngOnInit() {
-    this.form = this.formBuilder.group({
+    this.newPatientForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', Validators.required],
       birthday: ['', Validators.required],
+      cpf: ['', Validators.required],
       height: ['', Validators.required],
       weight: ['', Validators.required],
       sex: ['', Validators.required],
     });
   }
 
-  save() {
-    console.log(this.form)
+  savePatient() {
+    console.log(this.newPatientForm.value);
+
+    return this.service.postPatient(this.newPatientForm.value).subscribe(
+      data => console.log("Patient created!"),
+      error => console.log("error on savePatient", error)
+    )
+
+    
   }
 
 }
